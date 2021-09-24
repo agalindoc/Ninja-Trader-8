982b577829c44df9ccfe13efbdb9a5741a4abda4
Alejandro Galindo Chairez <69223009+agalindoc@users.noreply.github.com>
2021-09-23 22:07:14 -0500

    Update DivergenciesEngine.cs

--- indicators/DivergenciesEngine.cs
+++ indicators/DivergenciesEngine.cs
@@ -170,7 +170,6 @@ namespace NinjaTrader.NinjaScript.Indicators
             plotHiddenBull = true;
             plotBear = true;
             plotHiddenBear = true;
-            showDivergences = false;
             plotBullArrows = true;
             plotBearArrows = true;
             showDivergences = true;
